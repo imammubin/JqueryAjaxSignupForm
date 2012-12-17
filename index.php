430d6805d7311398de9763f2308370c88ba8ed74
mubinandroid <mubin.android@gmail.com>
2012-12-18 02:41:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,35 @@
 <html>
     <head> 
-        <title> Jquery Ajax Signup Form </title>
+        <title> Jquery Ajax Signup Form | www.imammubin.com </title>
         <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
     </head>
     <body>
         <h1> Signup Form </h1>
         
         <form name="signupForm" id="signupForm">
+            <input type="text" id="username" name="username" value="">
+            <input type="text" id="password" name="password" value="">
+            <input type="text" id="password2" name="password2" value="">
+            <input type="text" id="email" name="email" value="">
             <input type="submit" id="submit" value="submit">
         </form>
+        <div id="result"></div>
         
         <script type="text/javascript">
-        $(document).ready(function(){
+            $(document).ready(function(){
           
-          $("#submit").click(function(){
-            alert("Submit Form");
-          })
+                $("#submit").click(function(){
+                    var dataString=$("#signupForm").serialize();
+                        $.post("signupProses.php",dataString,function(data){
+                        if(data)=="ok"{
+                            $("#result").html("signup succes, please check your email..");
+                        }else{
+                            $("#result").html("signup fail..");
+                        }
+                    })
+                })
         
-        });
+            });
         </script>
         
         <div id="footer"> from: <a href="http://imammubin.com">http://imammubin.com</a></div>
